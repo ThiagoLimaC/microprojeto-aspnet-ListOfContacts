b5e07c4d35bda1a7487fc90246091b9b980f33b4
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-05-03 22:24:46 -0300

    Set Contact as default controller and update navigation
    Changed the default route in `Program.cs` to use `ContactController` as the default controller. Updated `_Layout.cshtml` to simplify navigation by removing "Home" and "Privacy" links from the navbar and footer. Added `ContactController` with an `Index` action to display a list of contacts. Created a new `Index.cshtml` view for listing contacts with a "Create" button.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using microprojeto_aspnet_ListOfContacts.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace microprojeto_aspnet_ListOfContacts.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactController(AppDbContext context)
+        {
+            this._context = context;    
+        }
+
+        public IActionResult Index()
+        {
+            var contacts = _context.Contacts.ToList();
+            return View(contacts);
+        }
+    }
+}
