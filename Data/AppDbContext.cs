3d64dfb0515efc39d018b73521c06904929e8481
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-05-03 21:51:11 -0300

    Integrate Entity Framework Core into the application
    - Added `AppDbContext` and configured EF Core in `Program.cs`.
    - Updated `appsettings.json` with a SQL Server connection string.
    - Included EF Core dependencies in the `.csproj` file.
    - Created `AppDbContext.cs` to define the database context.
    - Made minor formatting and encoding fixes in code files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace microprojeto_aspnet_ListOfContacts.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using microprojeto_aspnet_ListOfContacts.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
